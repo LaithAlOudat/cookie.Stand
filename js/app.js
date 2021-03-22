7b8a70b54f5fe9231554a7944fccfdd2c7af5697
LaithAlOudat <aloudat.laith@gmail.com>
2021-03-22 11:50:59 +0200

    adding html structure

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-'use strict'
+'use strict';
 
 let operatingHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 const seattle = {
-    location: 'Seattle',
-    minCust: 23,
-    maxCust: 65,
-    avgCookiesPerSales: 6.3,
+  location: 'Seattle',
+  minCust: 23,
+  maxCust: 65,
+  avgCookiesPerSales: 6.3,
 }
